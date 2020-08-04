607ab236ee54e7ce32e83768057c03166b5f0d3e
minas1986 <30533267+minas1986@users.noreply.github.com>
2020-08-04 11:05:31 +0300

    Added form and EntityListener

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,26 +2,92 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Conference;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
-    /**
-     * @Route("/hello/{name}", name="homepage")
-     * @param string $name
-     * @return Response
-     */
-    public function index(string $name = '')
-    {
-        $great = '';
+    private $twig;
+    private $entityManager;
 
-        if ($name) {
-            $great = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+    }
+
+
+
+    /**
+     * @Route("/conference", name="homepage")
+     * @param ConferenceRepository $conferenceRepository
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function index(ConferenceRepository $conferenceRepository)
+    {
+        return new Response($this->twig->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findAll()]));
+    }
+
+    /**
+     * @Route("/conference/{slug}", name="conference")
+     * @param Request $request
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @param $photoDir
+     * @return Response
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+
+                }
+                $comment->setPhotoFilename($filename);
+            }
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
         }
-        $content = '<html lang="en"><body>'.$great.'<img src="/images/construction.jpg" width="500" alt="img"/></body></html>';
-        return new Response($content);
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response($this->twig->render('conference/show.html.twig',
+            [
+
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+                'comment_form' => $form->createView()
+            ]
+        ));
     }
 }
